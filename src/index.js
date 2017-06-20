32d565dbbf37ba8bd5f0c65c68173a7b3026a0c4
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-06-19 22:30:19 -0600

    Commented out service worker and added TODO with instructions to enable service worker for production build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// TODO: Un-comment out line 5 and 9 in production build to enable serviceWorker
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
