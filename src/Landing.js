f100a52edeec703c899fd2a145c78f8c2495785b
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-07-11 22:49:25 -0500

    Added static landing message and set initial development styles

--- src/Landing.js
+++ src/Landing.js
@@ -6,9 +6,16 @@ import Basketball from './assets/new-hero-photo.jpeg'
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <img alt='' className="landing-hero-img" src={Basketball}></img>
-      </div>
+      <main>
+        <section>
+          <img alt='' className="landing-hero-img" src={Basketball}></img>
+        </section>
+        <aside className="lading-message">
+          <h2>Hello!</h2>
+          <h3>Welcom to my portfolio.</h3>
+          <h4>Start Exploring!</h4>
+        </aside>
+      </main>
     );
   }
 }
