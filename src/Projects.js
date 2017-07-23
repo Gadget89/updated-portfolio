cceac9ba266bcd60a24bcdf91b8f16dc930ff928
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-07-22 20:56:40 -0500

    Sudo coded goals for carousel in projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './main.css';
 
 class Projects extends Component {
+  // Create a Slide array
+  // Map through array and display one slide every 10 seconds
 
   render() {
     return (
       <section className="project-container">
-        
+
       </section>
     );
   }
