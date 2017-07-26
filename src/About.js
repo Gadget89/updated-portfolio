35622b9b6a817c96c7828a15450fba22d1a30982
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-07-26 18:49:55 -0500

    Added stacked paper background styling to About.js

--- src/About.js
+++ src/About.js
@@ -6,24 +6,30 @@ class About extends Component {
 
   render() {
     return (
-      <article id="about" className="about-container">
-        <h2 name="about" className="component-content-title">MY STORY</h2>
-        <p className="about-content">
-          I began my journey to become a software developer in the Fall of 2016. I've had the great privilege to learn from accomplished instructors at The Iron Yard and I graduated from their Front-End Engineering program in February 2017.
-        </p>
-        <p className="about-content">
-          After gaining a solid foundation, I am excited for this lifetime adventure. My curiosity and passion for code continue driving me to keep learning as much as I can.
-        </p>
-        <p className="about-content">
-          I would like to join a great team and be involved in projects that I can be proud of. I invite you to check out my projects and please feel free to contact me.
-        </p>
-        <section className="about-img-container">
-          <img
-            alt="Portrait of Nicolas Roybal."
-            src={Me}>
-          </img>
-        </section>
-      </article>
+      <section>
+        <article className="about-container-back"></article>
+        <article className="about-container-front"></article>
+          <article id="about" className="about-container">
+            <h2 name="about" className="component-content-title">MY STORY</h2>
+            <p className="about-content">
+              I began my journey to become a software developer in the Fall of 2016. I've had the great privilege to learn from accomplished instructors at The Iron Yard and I graduated from their Front-End Engineering program in February 2017.
+            </p>
+            <p className="about-content">
+              After gaining a solid foundation, I am excited for this lifetime adventure. My curiosity and passion for code continue driving me to keep learning as much as I can.
+            </p>
+            <p className="about-content">
+              I would like to join a great team and be involved in projects that I can be proud of. I invite you to check out my projects and please feel free to contact me.
+            </p>
+            <section className="about-img-container">
+              <img
+                alt="Portrait of Nicolas Roybal."
+                src={Me}>
+              </img>
+            </section>
+          </article>
+
+      </section>
+
     );
   }
 }
