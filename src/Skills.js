439b8027586d47148d3223d3347da14df3411d61
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-07-31 19:19:24 -0500

    Started creating arrays to hold skills list items

--- src/Skills.js
+++ src/Skills.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './main.css';
 
+const skillList = [
+  {
+    id: "",
+    title: "",
+    icon: "",
+    items: [
+      "test1",
+      "test2",
+
+    ]
+  }
+]
+
+
 class Skills extends Component {
 
   render() {
     return (
       <section>
-        <h2>CONNECTED!</h2>
       </section>
     );
   }
